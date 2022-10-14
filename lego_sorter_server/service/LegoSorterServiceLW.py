3ebe64356690bdf4a3b51c41abba7d44ad1caf2f
Damian Korneluk <damol9@wp.pl>
2022-10-14 19:16:22 +0200

    Update LegoSorterServiceLW.py

--- lego_sorter_server/service/LegoSorterServiceLW.py
+++ lego_sorter_server/service/LegoSorterServiceLW.py
@@ -69,7 +69,7 @@ class LegoSorterServiceLW(LegoSorterLW_pb2_grpc.LegoSorterLWServicer):
 
         return 
 	
-	def send_image_order(self):
+    def send_image_order(self):
         if self.deviceIP is not None and self.deviceIP != "":
             sock = socket(AF_INET, SOCK_DGRAM)  # UDP Socket
             logging.info(f"[LegoSorterServiceLW] Sending image order to client {self.deviceIP}.")
