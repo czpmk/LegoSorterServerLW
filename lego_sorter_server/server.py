3327a453c73d9c61b88d5c6f784d81a63806c9f0
czpmk <czapiewskimk@gmail.com>
2022-10-14 21:07:31 +0200

    remove redundant code; add AsyncSortingProcessor class

--- lego_sorter_server/server.py
+++ lego_sorter_server/server.py
@@ -1,11 +1,12 @@
 import grpc
 
 from concurrent import futures
-from lego_sorter_server.generated import LegoSorter_pb2_grpc, LegoSorterLW_pb2_grpc, LegoCapture_pb2_grpc, LegoAnalysis_pb2_grpc
+from lego_sorter_server.generated import LegoSorter_pb2_grpc, LegoAsyncSorter_pb2_grpc, LegoCapture_pb2_grpc, \
+    LegoAnalysis_pb2_grpc
 from lego_sorter_server.service.LegoCaptureService import LegoCaptureService
 from service.LegoAnalysisService import LegoAnalysisService
 from service.LegoSorterService import LegoSorterService
-from service.LegoAsyncSorterService import LegoSorterServiceLW
+from service.LegoAsyncSorterService import LegoAsyncSorterService
 
 from lego_sorter_server.service.BrickCategoryConfig import BrickCategoryConfig
 
@@ -13,15 +14,16 @@ from lego_sorter_server.service.BrickCategoryConfig import BrickCategoryConfig
 class Server:
 
     @staticmethod
-    def run(sorter_config: BrickCategoryConfig, enable_new_version: bool):
+    def run(sorter_config: BrickCategoryConfig, async_sorter: bool):
         options = [('grpc.max_receive_message_length', 100 * 1024 * 1024)]
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=16), options=options)
-        
-        if enable_new_version:
-            LegoSorterLW_pb2_grpc.add_LegoSorterLWServicer_to_server(LegoSorterServiceLW(sorter_config), server)
+
+        if async_sorter:
+            LegoAsyncSorter_pb2_grpc.add_LegoAsyncSorterServicer_to_server(LegoAsyncSorterService(sorter_config),
+                                                                           server)
         else:
             LegoSorter_pb2_grpc.add_LegoSorterServicer_to_server(LegoSorterService(sorter_config), server)
-        
+
         LegoCapture_pb2_grpc.add_LegoCaptureServicer_to_server(LegoCaptureService(), server)
         LegoAnalysis_pb2_grpc.add_LegoAnalysisServicer_to_server(LegoAnalysisService(), server)
         server.add_insecure_port('[::]:50051')
---
+++ lego_sorter_server/sorter/AsyncSortingProcessor.py
@@ -0,0 +1,21 @@
+import logging
+import time
+
+from typing import List, Tuple, Dict
+from PIL.Image import Image
+
+from lego_sorter_server.sorter.LegoSorterController import LegoSorterController
+
+
+class AsyncSortingProcessor:
+    def __init__(self, brick_category_config: BrickCategoryConfig):
+        self.sorter_controller: LegoSorterController = LegoSorterController(brick_category_config)
+
+    def start_machine(self):
+        self.sorter_controller.run_conveyor()
+
+    def stop_machine(self):
+        self.sorter_controller.stop_conveyor()
+
+    def set_machine_speed(self, speed: int):
+        self.sorter_controller.set_machine_speed(speed)
