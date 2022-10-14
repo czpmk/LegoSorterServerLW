4a5ca46800ef26f80ce645f6ec25d9db950a8d1f
czpmk <czapiewskimk@gmail.com>
2022-10-14 21:43:54 +0200

    add missing import

--- lego_sorter_server/sorter/AsyncSortingProcessor.py
+++ lego_sorter_server/sorter/AsyncSortingProcessor.py
@@ -4,6 +4,7 @@ import time
 from typing import List, Tuple, Dict
 from PIL.Image import Image
 
+from lego_sorter_server.service.BrickCategoryConfig import BrickCategoryConfig
 from lego_sorter_server.sorter.LegoSorterController import LegoSorterController
 
 
