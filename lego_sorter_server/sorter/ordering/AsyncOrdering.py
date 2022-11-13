2a592f3fdc28f13ab43132a632c043058f299658
czpmk <czapiewskimk@gmail.com>
2022-11-13 23:42:53 +0100

    bug fixes

--- lego_sorter_server/sorter/ordering/AsyncOrdering.py
+++ lego_sorter_server/sorter/ordering/AsyncOrdering.py
@@ -17,6 +17,7 @@ class AsyncOrdering:
         self.conveyor_state: OrderedDict[int, DetectionBox] = OrderedDict()
         '''Bricks on the conveyor belt, format - key: brick_id: int, value = detection_box: DetectionBox'''
 
+        # TODO: fix - head_brick_idx should be idx of the brick closest to the camera line, not last idx of the brick
         self.head_brick_idx = -1
         '''Index of first brick on the tape'''
         self.bricks: OrderedDict[int, AnalysisResultsList] = OrderedDict()
@@ -48,12 +49,14 @@ class AsyncOrdering:
 
         # check if any brick has passed the camera line - trigger sorter
         bricks_passed_the_camera_line = self._get_bricks_passed_the_camera_line(detection_results_list)
-        self._sort(bricks_passed_the_camera_line)
+        if len(bricks_passed_the_camera_line) > 0:
+            self._sort(bricks_passed_the_camera_line)
 
         self._prepare_for_classification(image_idx, detection_results_list)
 
     def _prepare_for_classification(self, image_idx: int, detection_results_list: DetectionResultsList):
-        image: Image = self.images.pop(image_idx)
+        # TODO: remove images once
+        image: Image = self.images[image_idx]
 
         previous_first_brick_id, previous_first_detection_box = self._get_first_brick_from_conveyor_state()
 
@@ -90,7 +93,7 @@ class AsyncOrdering:
         self.classification_worker.enqueue((brick_id, detection_id, cropped_image))
 
     def on_classification(self, brick_id: int, detection_id: int, classification_result: ClassificationResult):
-        self.bricks[brick_id][detection_id] = classification_result
+        self.bricks[brick_id][detection_id].merge_classification_result(classification_result)
 
     def on_sort(self, brick_id):
         # TODO: add logic - stats
@@ -107,27 +110,32 @@ class AsyncOrdering:
                     len(brick_id_list)))
 
         brick_id = brick_id_list[0]
-        self.sorting_worker.enqueue((brick_id, self._get_analysis_result(brick_id)))
+        analysis_result = self._get_analysis_result(brick_id)
+        if analysis_result is not None:
+            self.sorting_worker.enqueue((brick_id, analysis_result))
 
         # TODO: rest of the logic - stats
 
     def _get_analysis_result(self, brick_id) -> Optional[AnalysisResult]:
         analysis_results_list = self.bricks[brick_id]
-        if len(analysis_results_list) == 0:
+        analysis_results_list_classified: AnalysisResultsList = AnalysisResultsList(
+            [result for result in analysis_results_list if
+             result.classification_score is not None])
+
+        if len(analysis_results_list_classified) == 0:
             logging.warning('[AsyncOrdering] Attempting to sort a brick which lacks classification results')
             return None
 
         # get by best score
         # TODO: add other options
-        analysis_results_list.sort(key=lambda result: result.classification_score, reverse=True)
-        return analysis_results_list[0]
+        analysis_results_list_classified.sort(key=lambda result: result.classification_score, reverse=True)
+        return analysis_results_list_classified[0]
 
     def _get_bricks_passed_the_camera_line(self, current_detection_results: DetectionResultsList) -> List[int]:
         current_first_detection = current_detection_results[0] if len(current_detection_results) > 0 else None
 
         if current_first_detection is None:
             bricks_passed_camera_line = list(self.conveyor_state.keys())
-            self.conveyor_state.clear()
 
         else:
             bricks_passed_camera_line = list(filter(
