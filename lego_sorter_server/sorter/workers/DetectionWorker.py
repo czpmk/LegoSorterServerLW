78ec9d903a491d19a4db883da37f2c240d32b6a6
damol9 <admin@damol.pl>
2022-12-21 20:22:52 +0100

    image saving on async sorter

--- lego_sorter_server/sorter/workers/DetectionWorker.py
+++ lego_sorter_server/sorter/workers/DetectionWorker.py
@@ -1,18 +1,23 @@
 import logging
+import time
 from typing import Callable, Tuple
 
 from PIL.Image import Image
 
 from lego_sorter_server.analysis.AnalysisService import AnalysisService
+from lego_sorter_server.analysis.detection import DetectionUtils
 from lego_sorter_server.common.DetectionResults import DetectionResultsList
+from lego_sorter_server.images.storage.LegoImageStorage import LegoImageStorage
 from lego_sorter_server.sorter.workers.Worker import Worker
 
 
 class DetectionWorker(Worker):
-    def __init__(self, analysis_service: AnalysisService):
+    def __init__(self, analysis_service: AnalysisService, save_image=True):
         super().__init__()
 
         self.analysis_service: AnalysisService = analysis_service
+        self.save_image: bool = save_image
+        self.storage: LegoImageStorage = LegoImageStorage()
         self.set_target_method(self.__detect)
 
     def enqueue(self, item: Tuple[int, Image]):
@@ -26,4 +31,16 @@ class DetectionWorker(Worker):
 
         logging.debug('[{0}] Bricks detected {1} at image {2}.'.format(self._type(), len(detection_results_list),
                                                                        image_idx))
+
+        if self.save_image is True:
+            start_time_saving = time.time()
+            time_prefix = f"{int(start_time_saving * 10000) % 10000}"  # 10 seconds
+            for idx in range(len(detection_results_list)):
+                detection_box = detection_results_list[idx].detection_box
+                cropped_image = DetectionUtils.crop_with_margin(image, detection_box)
+                self.storage.save_image(cropped_image, str(idx), time_prefix)
+            self.storage.save_image(image, "async_sorter", time_prefix)
+            logging.info(
+                f"[{self._type()}] Saving images took {1000 * (time.time() - start_time_saving)} ms.")
+
         self._callback(image_idx, detection_results_list)
