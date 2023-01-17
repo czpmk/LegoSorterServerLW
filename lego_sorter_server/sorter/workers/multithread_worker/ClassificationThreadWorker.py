2abb3947761ef656758ec869a2a0afeb88650671
mc <czapiewskimk@gmail.com>
2023-01-17 17:20:36 +0100

    remove redundant imports

--- lego_sorter_server/sorter/workers/multithread_worker/ClassificationThreadWorker.py
+++ lego_sorter_server/sorter/workers/multithread_worker/ClassificationThreadWorker.py
@@ -1,10 +1,8 @@
 import logging
 from tkinter import Image
-from typing import Tuple, Callable, Optional
+from typing import Tuple, Callable
 
-from lego_sorter_server.common.ClassificationResults import ClassificationResult, ClassificationResultsList
-from lego_sorter_server.common.DetectionResults import DetectionResultsList
-from lego_sorter_server.sorter.workers.DetectionWorker import DetectionWorker
+from lego_sorter_server.common.ClassificationResults import ClassificationResultsList
 from lego_sorter_server.sorter.workers.multithread_worker.ThreadWorker import ThreadWorker
 
 
