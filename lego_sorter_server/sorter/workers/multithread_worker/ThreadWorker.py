ed5ab1746a18ee88ee2bda233d5680f439f2fbbf
mc <czapiewskimk@gmail.com>
2023-01-17 17:06:58 +0100

    move analysis service and sorter controller to ThreadWorker, as the fields won't be used in ProcessWorker

--- lego_sorter_server/sorter/workers/multithread_worker/ThreadWorker.py
+++ lego_sorter_server/sorter/workers/multithread_worker/ThreadWorker.py
@@ -4,14 +4,21 @@ from queue import Empty
 from threading import Thread
 from typing import Callable, Optional
 
-from lego_sorter_server.sorter.workers.Worker import Worker
+from lego_sorter_server.analysis.AnalysisService import AnalysisService
+from lego_sorter_server.sorter.LegoSorterController import LegoSorterController
+from lego_sorter_server.sorter.workers.Worker import Worker, WorkerMode
 
 
 class ThreadWorker(Worker):
     def __init__(self):
         super().__init__()
+        self.mode = WorkerMode.Thread
         self.thread: Thread = Thread()
         self.target_method: Optional[Callable] = None
+        self.running: bool = False
+
+        self.analysis_service: Optional[AnalysisService] = None
+        self.sorter_controller: Optional[LegoSorterController] = None
 
     def start(self):
         if self.running is True:
@@ -30,9 +37,10 @@ class ThreadWorker(Worker):
         self.running = False
         if self.thread.is_alive():
             self.thread.join(1)
+
         logging.info('[{0}] Stopping thread (Queue size: {1})'.format(self._type(), self.input_queue.qsize()))
 
-    def _clear_queue(self):
+    def clear_queue(self):
         logging.info('[{0}] Clearing queue (Queue size: {1})'.format(self._type(), self.input_queue.qsize()))
         with self.input_queue.mutex:
             self.input_queue.queue.clear()
@@ -41,8 +49,14 @@ class ThreadWorker(Worker):
         if self.running:
             self.stop()
 
-        self._clear_queue()
+        self.clear_queue()
         self.start()
+
+    def set_analysis_service(self, analysis_service: AnalysisService):
+        self.analysis_service = analysis_service
+
+    def set_sorter_controller(self, sorter_controller: LegoSorterController):
+        self.sorter_controller = sorter_controller
 
     def run(self):
         while self.running:
